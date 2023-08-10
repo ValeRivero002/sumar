129fa3e161ceb4a01683ca25635ea2454e33874e
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2023-08-10 06:38:08 -0400

    Mis nuevos cambios

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,14 @@
+import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
-
+const first2 = document.querySelector("#primer-numero2");
+const second2 = document.querySelector("#segundo-numero2");
+const formMulti = document.querySelector("#multiplicar-form");
+const divMulti = document.querySelector("#resultadoMulti-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -12,4 +16,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formMulti.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber2 = Number.parseInt(first2.value);
+  const secondNumber2 = Number.parseInt(second2.value);
+
+  divMulti.innerHTML = "<p>" + multiplicar(firstNumber2, secondNumber2) + "</p>";
 });
